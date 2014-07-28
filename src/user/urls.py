57133af80a1a242e38a0ee47ed243e352bb465f9
Askhat Omarov <askhat.omarov91@gmail.com>
2014-07-28 17:34:23 +0600

    profile view

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
-from user.views import UserListView
+from user.views import UserListView, UserProfileView, user_profile_settings
+
 from user.models import User
 
 urlpatterns = patterns(
@@ -7,4 +8,7 @@ urlpatterns = patterns(
     url(r'^list/$', UserListView.as_view(
         model=User,
         template_name='user/user_list.html',
-        queryset=User.objects.all())))
+        queryset=User.objects.all())),
+    url(r'^profile/$', UserProfileView.as_view(template_name='user/profile.html'), name='profile_url'),
+    url(r'^profile/settings/$', user_profile_settings, {'template_name':'user/profile_settings.html'}, name='profile_settings_url')
+)
--- src/user/views.py
+++ src/user/views.py
@@ -1,5 +1,27 @@
 from django.views.generic import ListView
+from django.views.generic.base import TemplateView
+from django.template.response import TemplateResponse
+
+from user.models import User
 
 
 class UserListView(ListView):
     pass
+
+class UserProfileView(TemplateView):
+	"""
+	Shows users profile info
+	"""
+
+	# should be:
+	# user = request.user
+
+	def get_context_data(self, **kwargs):
+		ctx = super(UserProfileView, self).get_context_data(**kwargs)
+		# should be:
+		# kwargs['user'] = self.request.user
+		ctx['user'] = User.objects.get(id=1)
+		return ctx
+
+def user_profile_settings():
+	pass
