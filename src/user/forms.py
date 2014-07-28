b1d4189501fd544974e33d7e4e1577c8f26fffae
Askhat Omarov <askhat.omarov91@gmail.com>
2014-07-29 00:16:27 +0600

    indentation with spaces

--- src/user/forms.py
+++ src/user/forms.py
@@ -7,40 +7,40 @@ from user.models import User
 
 class UserBaseSettingsForm(ModelForm):
 
-	class Meta:
-		model = User
-		fields = ['first_name', 'last_name', 'city', 'country']
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'city', 'country']
 
 class UserPasswordSettingsForm(forms.Form):
 
-	old_password = forms.CharField(widget=forms.PasswordInput, label=_("Old password"))
-	password = forms.CharField(widget=forms.PasswordInput(), label=_('New Password'))
-	password2 = forms.CharField(widget=forms.PasswordInput(), label=_('Repeat your password'))
+    old_password = forms.CharField(widget=forms.PasswordInput, label=_("Old password"))
+    password = forms.CharField(widget=forms.PasswordInput(), label=_('New Password'))
+    password2 = forms.CharField(widget=forms.PasswordInput(), label=_('Repeat your password'))
 
-	error_messages = {
-		'password_mismatch': _("New passwords are not equal"),
-		'old_password_incorrect': _("Enter valid old password.")
-	}
+    error_messages = {
+        'password_mismatch': _("New passwords are not equal"),
+        'old_password_incorrect': _("Enter valid old password.")
+    }
 
-	def __init__(self, user, *args, **kwargs):
-		self.user = user
-		super(UserPasswordSettingsForm, self).__init__(*args, **kwargs)
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super(UserPasswordSettingsForm, self).__init__(*args, **kwargs)
 
-	def clean_old_password(self):
-		old_password = self.cleaned_data['old_password']
-		if not check_password(old_password, self.user.password):
-		    raise forms.ValidationError(self.error_messages['old_password_incorrect'])
-		return old_password
+    def clean_old_password(self):
+        old_password = self.cleaned_data['old_password']
+        if not check_password(old_password, self.user.password):
+            raise forms.ValidationError(self.error_messages['old_password_incorrect'])
+        return old_password
 
-	def clean(self):
-		password = self.cleaned_data['password']
-		password2 = self.cleaned_data['password2']
-		if password and password2:
-			if password != password2:
-				raise forms.ValidationError(self.error_messages['password_mismatch'])
-		return self.cleaned_data
+    def clean(self):
+    password = self.cleaned_data['password']
+    password2 = self.cleaned_data['password2']
+    if password and password2:
+        if password != password2:
+            raise forms.ValidationError(self.error_messages['password_mismatch'])
+    return self.cleaned_data
 
-	def save(self, *args, **kwargs):
-		self.user.set_password(self.cleaned_data['password'])
-		self.user.save()
-		return self.user
+    def save(self, *args, **kwargs):
+        self.user.set_password(self.cleaned_data['password'])
+        self.user.save()
+        return self.user
