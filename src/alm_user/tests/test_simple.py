47da1de85b4c09321ba26ff9527c658b2e993677
Yernar Mailyubayev <mailubai@ubuntu.(none)>
2014-07-31 06:30:29 -0700

    selenium configured

--- src/alm_user/tests/test_simple.py
+++ src/alm_user/tests/test_simple.py
@@ -1,6 +1,9 @@
 from nose2.tools import such, params
 from alm_user.models import User
 from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 
 
 class AlmaUserLayer(object):
@@ -10,6 +13,10 @@ class AlmaUserLayer(object):
     def setUp(cls):
         it.url = 'http://bb.co.uk/user/signin'
         it.email = "r.kamun@gmail.com"
+        #driver = webdriver.Firefox()
+        it.driver = webdriver.Remote(
+            command_executor='http://192.168.233.1:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.FIREFOX)
 
     @classmethod
     def tearDown(cls):
@@ -31,6 +38,11 @@ with such.A('Global auth system') as it:
     @it.should('say hi')
     def test():
         it.assertEqual(it.email, 'r.kamun@gmail.com')
+
+    @it.should('ask a google')
+    def test_selenium():
+        it.driver.get("http://www.google.com")
+        it.assertIn("Google", it.driver.title)
 
 
 it.createTests(globals())
