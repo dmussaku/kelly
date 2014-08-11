a53458fad575e8cd67e0055360ad567be25f8091
Daniyar <dmussaku@gmail.com>
2014-08-11 12:51:29 +0600

    contact started

---
+++ src/alm_contact/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+#Contact model: (first_name, last_name, company_name, phone, email)
+class Contact(models.Model):
+	first_name = models.CharField(_('first name'), max_length=31,
+                                  null=False, blank=False)
+    last_name = models.CharField(_('last name'), max_length=30, blank=False)
+    company_name = models.CharField(_('company name'), max_length=50, blank=True)
+    phone = models.CharField(_('phone'), max_length=12, blank=True)
+    email = models.EmailField(_('email address'), unique=True, blank=False)
+
+    class Meta:
+        verbose_name = _('user')
+        db_table = settings.DB_PREFIX.format('user')
