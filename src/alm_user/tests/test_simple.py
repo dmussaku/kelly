4d3415a4499c81026e5aede5e837ba8cd7a2805d
Yernar Mailyubayev <mailubai@ubuntu.(none)>
2014-08-14 04:54:41 -0700

    new tests from Alibek

--- src/alm_user/tests/test_simple.py
+++ src/alm_user/tests/test_simple.py
@@ -77,7 +77,7 @@ with such.A('Alma.net Registration') as it:
         MainSeleniumLayer.share_vars(it)
         it.current_user = test_user()
 
-        it.registration_url = it.app_url + '/auth/signup'
+        it.registration_url = it.app_url + reverse('user_registration')
 
 
         it.driver.get(it.registration_url)
@@ -114,7 +114,7 @@ with such.A('Alma.net Registration') as it:
         def setup():
             # Steps 1.2.3. are part of setup. 4 a.b.c.d. and 5 is what to be tested
             # Setting up 1.
-            it.login_url = it.app_url + "/auth/signin"
+            it.login_url = it.app_url + reverse('user_login')
             it.current_user = create_test_user()
             it.driver.get(it.login_url)
             # locate username field
@@ -170,7 +170,7 @@ with such.A('Alma.net Registration') as it:
 
         @it.has_setup
         def setup():
-            it.login_url = it.app_url + "/auth/signin"
+            it.login_url = it.app_url + reverse('user_login')
             it.current_user = create_inactive_test_user()
             # locate username field
             elem = it.driver.find_element_by_name('username')
@@ -303,6 +303,159 @@ with such.A('Alma.net Registration') as it:
             # check the title page
             it.assertIn('password reset', it.driver.title.lower())
 
+
+
+
+    # SECOND SET OF REQS
+    """
+    ADDED 14/08/2014
+
+    View user profile
+
+    Precondition: user has active session (he is logged in)
+
+    User opens profile page e.g. profile
+    User can preview profile page with his
+        Basic information: first_name, last_name
+        Preferences: timezone, language
+        Available Services (links to available services)
+        Billing information
+        Credit card details
+    """
+    # PREP TEST TO MAKE SURE WE'RE LOGGED IN 
+    with it.having('logging in'):
+
+        @it.has_setup
+        def setup():
+            it.login_url = it.app_url + reverse('user_login')
+            it.current_user = test_user()
+            # login
+            it.driver.get(it.login_url)
+            # find the field and fill it in
+            elem = it.driver.find_element_by_name('username')
+            elem.send_keys(it.current_user.email)
+            # find the field and fill it in
+            elem = it.driver.find_element_by_name('password')
+            elem.send_keys('123')
+            # click on submit button
+            it.driver.find_element_by_id('id_submit').click()
+
+        @it.should('Redirect to profile page')
+        def test():
+            it.assertIn('user profile', it.driver.title.lower())
+
+        @it.should('have certain fields')
+        def test():
+            # fields to compare against
+            stack_of_fields = ["Company", "Country", "City", "Email", "Last name", "First name"]
+            # getting all elements using xpath
+            all_elements = it.driver.find_elements_by_css_selector('li')
+            # check against stack of specified fields 
+            for elem in all_elements:
+                field = stack_of_fields.pop()
+                it.assertIn(field, elem.text)
+
+        @it.should('NOT have certain fields')
+        def test():
+            # fields to compare against
+            stack_of_fields = ["Fizz", "Buzz", "FizzBuzz", "Foo", "Bar", "FooBar"]
+            # getting all elements using xpath
+            all_elements = it.driver.find_elements_by_css_selector('li')
+            # check against stack of specified fields 
+            for elem in all_elements:
+                field = stack_of_fields.pop()
+                it.assertNotEqual(field, elem.text)
+
+        @it.should('could view what time it is')
+        def test():
+            s = "Your time is:"
+            elem = it.driver.find_element_by_css_selector('p')
+            it.assertIn(s, elem.text)
+            # NOT SURE HOW TO TEST IF ACTUAL TIME IS DISPLAYED
+
+        @it.should('have a link to connected services')
+        def test():
+            s = "View connected services"
+            # find href with services text link
+            # this makes sure that it is a link bc we're using partial_link_text 
+            #                                    and it has the specified string
+            elem = it.driver.find_element_by_partial_link_text('services')
+            it.assertIn(s, elem.text)
+
+    # YET TO DO BILLING INFO AND CREDIT CARD INFO TESTS. 
+    # TO BE DONE AFTER CLARIFYING EXACT REQS WITH YERNAR. 
+
+
+
+
+
+    # THIRD SET OF REQS
+    """
+    Edit user profile
+        Precondition: user has active session (he is logged in)
+
+        1. User enters profile edit page e.g. profile/edit
+        2. User updates fields and submits
+        3. User redirected to profile e.g. profile
+        4. User notified with messages that he successfully updated profile
+    """
+    # AT THIS POINT WE'RE STILL LOGGED IN AFTER THE PREVIOUS TESTS
+    # RUN A PRETEST JUST TO MAKE SURE WE'RE LOGGED IN
+
+    with it.having('user is still logged in'):
+
+        @it.should('be in user profile page')
+        def test():
+            it.assertIn('user profile', it.driver.title.lower())
+            
+    with it.having('clicked the settings link on profile page'):
+
+        @it.has_setup
+        def setup():
+            # go to the settings page. could be accomplished in 2 ways, just clicking settings or
+            # going to specific url that directs us to user settings page. Chose the first. 
+            elem = it.driver.find_element_by_link_text('Settings')
+            elem.click()
+        
+        @it.should('go to setttings page')
+        def test():
+            it.assertIn('user profile settings page', it.driver.title.lower())
+
+    # THIS TEST NEEDS TO BE WORKED ON
+    with it.having('being on the settings page'):
+
+        @it.has_setup
+        def setup():
+            first_name = "Yernar"
+            last_name = "Mailubai"
+            # changing name fields
+            elem = it.driver.find_element_by_name('first_name')
+            # clears out field from previous value, i.e. different first name to be changed
+            elem.clear()
+            elem.send_keys(first_name)
+            elem = it.driver.find_element_by_name('last_name')
+            # clears out field from previous value, i.e. different last name to be changed
+            elem.clear()
+            elem.send_keys(last_name)
+            # changing the timezone field
+
+            # find submit button
+            xpath = "//input[@type='submit']"
+            elem = it.driver.find_element_by_xpath(xpath)
+            elem.click()
+
+        @it.should('successfully fill out name fields and redirects to user profile')
+        def test():
+            # as soon as user updates fields and submits -- redirect to profile page
+            it.assertIn('user profile', it.driver.title.lower())
+
+
+        @it.should('should contain headline - notifies of successfully updated profile')
+        def test():
+            # find headline which contains message
+            elem = it.driver.find_elements_by_css_selector('h2')
+            message = "Your information was updated successfully"
+            it.assertEqual(message, elem.text)
 
 
 it.createTests(globals())
