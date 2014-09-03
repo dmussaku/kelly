373415b50cbae7f20e64b8b76e6784b824a49906
aidaroff <aydarov.alibek@gmail.com>
2014-09-03 16:40:42 +0600

    Value CRUD: Done

--- src/almanet/urls.py
+++ src/almanet/urls.py
@@ -5,12 +5,17 @@ from almanet.views import fork_index
 from almanet.views import (
     TestView2,
     ProductList,
+    ValueList,
     connect_product,
     disconnect_product,
     ProductCreateView,
     ProductUpdateView,
     ProductDeleteView,
-    ProductDetailView,)
+    ProductDetailView,
+    ValueCreateView,
+    ValueUpdateView,
+    ValueDeleteView,
+    ValueDetailView,)
 
 urlpatterns = patterns(
     '',
@@ -20,6 +25,9 @@ urlpatterns = patterns(
     url(r'^products/$', ProductList.as_view(
         template_name='almanet/product_list.html'),
         name='product_list'),
+    url(r'^values/$', ValueList.as_view(
+        template_name='almanet/value_list.html'),
+        name='value_list'),
     url(r'^products/connect/(?P<slug>\w+)/$', connect_product,
         name='connect_product'),
     url(r'^products/disconnect/(?P<slug>\w+)/$', disconnect_product,
@@ -28,7 +36,12 @@ urlpatterns = patterns(
     url(r'^products/product_create/$', ProductCreateView.as_view(), name='product_create'),
     url(r'^products/product_update/(?P<pk>\d+)/$', ProductUpdateView.as_view(), name='product_update'),
     url(r'^products/product_detail/(?P<pk>\d+)/$', ProductDetailView.as_view(), name='product_detail'),
-    url(r'^products/product_delete/(?P<pk>\d+)/$', ProductDeleteView.as_view(), name='product_delete')
+    url(r'^products/product_delete/(?P<pk>\d+)/$', ProductDeleteView.as_view(), name='product_delete'),
+
+    url(r'^values/value_create/$', ValueCreateView.as_view(), name='value_create'),
+    url(r'^values/value_update/(?P<pk>\d+)/$', ValueUpdateView.as_view(), name='value_update'),
+    url(r'^values/value_detail/(?P<pk>\d+)/$', ValueDetailView.as_view(), name='value_detail'),
+    url(r'^values/value_delete/(?P<pk>\d+)/$', ValueDeleteView.as_view(), name='value_delete')
     # url(r'^admin/', include(admin.site.urls)),
 )
 
