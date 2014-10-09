3e5b1ea54768919a66d12e80a3f66d70b0a66e8a
Daniyar <dmussaku@gmail.com>
2014-10-09 13:06:57 +0600

    Made some minor changes

--- src/alm_vcard/views.py
+++ src/alm_vcard/views.py
@@ -13,6 +13,7 @@ from django.shortcuts import render_to_response, render
 from django.core.files.temp import NamedTemporaryFile
 from django.core.servers.basehttp import FileWrapper
 
+
 def filename_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
@@ -27,9 +28,10 @@ def import_vcard(request, **kwargs):
         form = VCardUploadForm(request.POST, request.FILES)
         if form.is_valid():
             data_list = request.FILES['myfile'].read().split('END:VCARD')[:-1]
+            contact_ids=[]
             for i in range(0, len(data_list)):
                 data_list[i] += 'END:VCARD'
-                Contact.upload_contacts('vcard', data_list[i], save=True)
+                contact_ids.append(Contact.upload_contacts('vcard', data_list[i], save=True).id)
             return HttpResponseRedirect('/almcrm/')
     else:
         form = VCardUploadForm()
