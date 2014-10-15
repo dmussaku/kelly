2bb6cfdf5dbf1a8a2ac473df821bc0c8bce53e2e
Daniyar <dmussaku@gmail.com>
2014-10-15 16:07:53 +0600

    Now twitter typeahead works, now need to change the hardcoded url and make links out of found contacts to redirect to contact page

---
+++ src/static/js/contact_search.js
@@ -0,0 +1,29 @@
+var result_list = function(data){
+    var result_list = [];
+    for (var i in data){
+      result_list.push({value:data[i].name});
+    }
+    console.log(result_list);
+    return result_list;
+  };
+
+$(document).ready(function(){
+  $('#scrollable-dropdown-menu .typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+  {
+    name: 'states',
+    displayKey: 'name',
+    source: function(query, process){
+      return $.ajax({
+        type:'GET',
+        url:'http://bwayne.alma.net:8000/almcrm/contacts/search/'+query+'/',
+        success: function(data){
+          return process(data);
+        }
+      });
+    }
+  });
+})
