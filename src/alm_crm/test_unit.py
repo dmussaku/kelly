6193d177bb93dc731812c28252ab8a8da25f3689
Abyken Nurlan <abyken.nurlan@gmail.com>
2015-02-04 16:20:50 +0600

    changes in test

--- src/alm_crm/test_unit.py
+++ src/alm_crm/test_unit.py
@@ -443,9 +443,10 @@ class SalesCycleTestCase(TestCase):
         owner = contact.owner
         subscription_id = owner.subscription_id
         count = SalesCycle.objects.all().count()
-        self.assertFalse(SalesCycle.create_globalcycle(**{'subscription_id': subscription_id,
+        self.assertEqual(SalesCycle.create_globalcycle(**{'subscription_id': subscription_id,
                                                          'owner_id': owner.id,
-                                                         'contact_id': contact.id}))
+                                                         'contact_id': contact.id}),
+                        contact.sales_cycles.get(is_global=True))
         actual_count = SalesCycle.objects.all().count()
         expected_count = count
         self.assertEqual(actual_count, expected_count)
