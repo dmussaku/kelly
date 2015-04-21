32adc0598388155f3557067e6f7182f4d53325de
Askhat Omarov <askhat.omarov91@gmail.com>
2015-04-21 13:59:04 +0600

    report construction changed: milestone on sales_cycles

--- src/alm_crm/utils/report_builders.py
+++ src/alm_crm/utils/report_builders.py
@@ -11,15 +11,15 @@ def build_funnel(subscription_id):
 	sales_cycles = SalesCycle.objects.filter(subscription_id=subscription_id, is_global=False)
 	rv['total'] = len(sales_cycles)
 
-	activities = map(lambda sc: sc.find_latest_activity(), sales_cycles)
-	rv['undefined'] = len(filter(lambda a: a == None or a.milestone == None, activities))
+	rv['undefined'] = len(filter(lambda sc: sc.milestone == None, sales_cycles))
 	
 	rv['funnel'] = {}
 	milestones = Milestone.objects.filter(subscription_id=subscription_id)
-	act_in_funnel = [a for a in activities if a != None and a.milestone != None]
+	
+	sc_in_funnel = [sc for sc in sales_cycles if sc.milestone != None]
 	for m in milestones:
-		rv['funnel'][m.id] = len(act_in_funnel)
-		act_in_funnel = [a for a in act_in_funnel if a.milestone.id != m.id]
+		rv['funnel'][m.id] = len(sc_in_funnel)
+		sc_in_funnel = [sc for sc in sc_in_funnel if sc.milestone.id != m.id]
 	return rv
 
 def build_realtime_funnel(subscription_id):
@@ -29,13 +29,13 @@ def build_realtime_funnel(subscription_id):
 	sales_cycles = SalesCycle.objects.filter(subscription_id=subscription_id, is_global=False)
 	rv['total'] = len(sales_cycles)
 
-	activities = map(lambda sc: sc.find_latest_activity(), sales_cycles)
-	rv['undefined'] = len(filter(lambda a: a == None or a.milestone == None, activities))
+	rv['undefined'] = len(filter(lambda sc: sc.milestone == None, sales_cycles))
 	
 	rv['funnel'] = {}
 	milestones = Milestone.objects.filter(subscription_id=subscription_id)
-	act_in_funnel = [a for a in activities if a != None and a.milestone != None]
+	
+	sc_in_funnel = [sc for sc in sales_cycles if sc.milestone != None]
 	for m in milestones:
-		rv['funnel'][m.id] = len([a for a in act_in_funnel if a.milestone.id == m.id])
+		rv['funnel'][m.id] = len([sc for sc in sc_in_funnel if sc.milestone.id == m.id])
 	return rv
 		
