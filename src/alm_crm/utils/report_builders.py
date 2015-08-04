9d291e124a70e711a9f1635e978b77890adece1a
Askhat Omarov <askhat.omarov91@gmail.com>
2015-08-04 13:13:20 +0600

    another product report construction approach

--- src/alm_crm/utils/report_builders.py
+++ src/alm_crm/utils/report_builders.py
@@ -120,74 +120,80 @@ def build_user_report(subscription_id, data):
 		'to_date': to_date}
 	return user_report
 
-def build_product_report(subscription_id, data):
-	product_ids=data.get('products', [-1])
-	print product_ids
-	from_date=data.get('from_date', None)
-	to_date=data.get('to_date', None)
-	if from_date == None:
-		from_date = datetime(2014, 1, 1).replace(tzinfo=pytz.UTC)
+def build_product_report(subscription_id, data={}):
 
-	if to_date == None:
-		to_date = datetime.now().replace(tzinfo=pytz.UTC)
+	rv = {
+		'report_name': 'product_report',
+		'products': {},
+	}
+	products = Product.objects.filter(subscription_id=subscription_id)
 
-	products_amount = len(product_ids) if len(product_ids) > 1 else Product.objects.filter(subscription_id=subscription_id).count()
-	open_sales_cycles = SalesCycle.objects.filter(
-							products__in = product_ids if product_ids[0] != -1 
-							else Product.objects.filter(subscription_id=subscription_id).values_list('id', flat=True),
-							milestone__is_system=0, is_global=False, date_created__range=(from_date, to_date)
-						)
+	for product in products:
+		rv['products'][product.id] = {
+			'earned_money': sum(SalesCycleProductStat.objects.filter(
+									subscription_id=subscription_id,
+									product_id=product.id,
+									sales_cycle__milestone__is_system=1).values_list('real_value', flat=True)),
+			'total_cycles': SalesCycle.objects.filter(products__pk__in=[product.id],
+															subscription_id=subscription_id,
+															is_global=False).count()
+		}
+	
+	# open_sales_cycles = SalesCycle.objects.filter(
+	# 						products__in = product_ids if product_ids[0] != -1 
+	# 						else Product.objects.filter(subscription_id=subscription_id).values_list('id', flat=True),
+	# 						milestone__is_system=0, is_global=False, date_created__range=(from_date, to_date)
+	# 					)
 
-	closed_sales_cycles = SalesCycle.objects.filter(
-								products__in = product_ids if product_ids[0] != -1 
-								else Product.objects.filter(subscription_id=subscription_id).values_list('id', flat=True),
-								milestone__is_system__in=[1,2],
-								is_global=False, date_created__range=(from_date, to_date)
-							)
-	successfull_cycles = closed_sales_cycles.filter(milestone__is_system=1)
-	unsuccessfull_cycles = closed_sales_cycles.filter(milestone__is_system=2)
+	# closed_sales_cycles = SalesCycle.objects.filter(
+	# 							products__in = product_ids if product_ids[0] != -1 
+	# 							else Product.objects.filter(subscription_id=subscription_id).values_list('id', flat=True),
+	# 							milestone__is_system__in=[1,2],
+	# 							is_global=False, date_created__range=(from_date, to_date)
+	# 						)
+	# successfull_cycles = closed_sales_cycles.filter(milestone__is_system=1)
+	# unsuccessfull_cycles = closed_sales_cycles.filter(milestone__is_system=2)
 
-	earned_money = sum(SalesCycleProductStat.objects.filter(sales_cycle__in=closed_sales_cycles).values_list('real_value', flat=True))
+	# earned_money = sum(SalesCycleProductStat.objects.filter(sales_cycle__in=closed_sales_cycles).values_list('real_value', flat=True))
 	
 
-	if product_ids == [-1]:
-		products = Product.objects.filter(subscription_id=subscription_id, date_created__range=(from_date, to_date))
-	else:
-		products = Product.objects.filter(subscription_id=subscription_id, id__in=product_ids, date_created__range=(from_date, to_date))
+	# if product_ids == [-1]:
+	# 	products = Product.objects.filter(subscription_id=subscription_id, date_created__range=(from_date, to_date))
+	# else:
+	# 	products = Product.objects.filter(subscription_id=subscription_id, id__in=product_ids, date_created__range=(from_date, to_date))
 
-	product_stat_array = []
+	# product_stat_array = []
 
-	for product in products:
-		product_obj = {}
-		product_obj['id'] = product.id
-		obj = []
-		for prod_stat in product.salescycleproductstat_set.all():
-			obj.append({'date_edited':prod_stat.date_edited, 'real_value':prod_stat.real_value})
-		product_obj['object'] = obj
-		product_stat_array.append(product_obj)
+	# for product in products:
+	# 	product_obj = {}
+	# 	product_obj['id'] = product.id
+	# 	obj = []
+	# 	for prod_stat in product.salescycleproductstat_set.all():
+	# 		obj.append({'date_edited':prod_stat.date_edited, 'real_value':prod_stat.real_value})
+	# 	product_obj['object'] = obj
+	# 	product_stat_array.append(product_obj)
 
-	product_array = []
-	for product in products:
-		product_obj = {}
-		product_obj['id'] = product.id
-		obj = {}
-		obj['open_sales_cycles'] = open_sales_cycles.filter(products__in=[product.id]).count()
-		obj['closed_sales_cycles'] = closed_sales_cycles.filter(products__in=[product.id]).count()
-		obj['successfull'] = closed_sales_cycles.filter(products__in=[product.id], milestone__is_system=1).count()
-		obj['unsuccessfull'] = closed_sales_cycles.filter(products__in=[product.id], milestone__is_system=2).count()
-		product_obj['stats'] = obj
-		product_array.append(product_obj)
-	user_report = {
-		'report_name': 'product_report',
-		'product_ids': product_ids if not product_ids[0] == -1 else None,
-		'products_amount': products_amount,
-		'open_sales_cycles':open_sales_cycles.count(),
-		'closed_sales_cycles':closed_sales_cycles.count(),
-		'earned_money': earned_money,
-		'product_stat_array':product_stat_array,
-		'product_array':product_array,
-		'successfull_cycles': successfull_cycles.count(),
-		'unsuccessfull_cycles': unsuccessfull_cycles.count(),
-		'from_date': from_date,
-		'to_date': to_date}
-	return user_report
+	# product_array = []
+	# for product in products:
+	# 	product_obj = {}
+	# 	product_obj['id'] = product.id
+	# 	obj = {}
+	# 	obj['open_sales_cycles'] = open_sales_cycles.filter(products__in=[product.id]).count()
+	# 	obj['closed_sales_cycles'] = closed_sales_cycles.filter(products__in=[product.id]).count()
+	# 	obj['successfull'] = closed_sales_cycles.filter(products__in=[product.id], milestone__is_system=1).count()
+	# 	obj['unsuccessfull'] = closed_sales_cycles.filter(products__in=[product.id], milestone__is_system=2).count()
+	# 	product_obj['stats'] = obj
+	# 	product_array.append(product_obj)
+	# user_report = {
+	# 	'report_name': 'product_report',
+	# 	'product_ids': product_ids if not product_ids[0] == -1 else None,
+	# 	'open_sales_cycles':open_sales_cycles.count(),
+	# 	'closed_sales_cycles':closed_sales_cycles.count(),
+	# 	'earned_money': earned_money,
+	# 	'product_stat_array':product_stat_array,
+	# 	'product_array':product_array,
+	# 	'successfull_cycles': successfull_cycles.count(),
+	# 	'unsuccessfull_cycles': unsuccessfull_cycles.count(),
+	# 	'from_date': from_date,
+	# 	'to_date': to_date}
+	return rv
