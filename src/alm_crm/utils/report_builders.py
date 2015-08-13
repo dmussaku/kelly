8fbca8f255a81ed0fa325c5f9378c3e8ee4a98f6
Abyken Nurlan <abyken.nurlan@gmail.com>
2015-08-13 14:00:43 +0600

    ready v5

--- src/alm_crm/utils/report_builders.py
+++ src/alm_crm/utils/report_builders.py
@@ -170,8 +170,7 @@ def get_activity_feed_xls(subscription_id, data=None):
 	worksheet.set_column(3, 3, 25)
 	worksheet.set_column(4, 4, 25)
 
-	workbook.close()
-	return workbook
+	return f
 
 		
 def build_user_report(subscription_id, data):
