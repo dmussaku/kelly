cc5fa1142ea7c1c270664f955e7a945656a7895c
Askhat Omarov <askhat.omarov91@gmail.com>
2015-12-02 14:35:58 +0600

    deleted extra sorting on sales_cycle api

--- src/alm_crm/apii/sales_cycles.py
+++ src/alm_crm/apii/sales_cycles.py
@@ -60,7 +60,7 @@ class SalesCycleViewSet(CompanyObjectAPIMixin, viewsets.ModelViewSet):
 
     @list_route(methods=['get'], url_path='all')
     def get_all(self, request, *args, **kwargs):    
-        sales_cycles = SalesCycle.get_all(company_id=request.company.id).order_by('-latest_activity__date_created', 'date_created')
+        sales_cycles = SalesCycle.get_all(company_id=request.company.id)
         sales_cycles = self.filter_class(request.GET, sales_cycles)
         page = self.paginate_queryset(sales_cycles)
         if page is not None:
@@ -72,7 +72,7 @@ class SalesCycleViewSet(CompanyObjectAPIMixin, viewsets.ModelViewSet):
 
     @list_route(methods=['get'], url_path='new/all')
     def get_new_all(self, request, *args, **kwargs):	
-    	sales_cycles = SalesCycle.get_new_all(company_id=request.company.id).order_by('-date_created')
+    	sales_cycles = SalesCycle.get_new_all(company_id=request.company.id)
         sales_cycles = self.filter_class(request.GET, sales_cycles)
     	page = self.paginate_queryset(sales_cycles)
         if page is not None:
